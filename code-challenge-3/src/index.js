a66da225adf6bfb90d395a3470394d3bfac51fc8
mbuthia wanjiru <sheezwagichuru.gmail.com>
2024-04-08 19:05:27 +0300

    week3

--- code-challenge-3/src/index.js
+++ code-challenge-3/src/index.js
@@ -1,79 +1,79 @@
 // Your code here
-const baseURL = "http://localhost:3000/films";
+const filmsEndpoint = "http://localhost:3000/films";
 
 // Variable declarations to reference different HTML elements on the page
-const filmListElem = document.getElementById("films");
-const moviePosterElem = document.getElementById("poster");
-const movieTitleElem = document.getElementById("title");
-const durationElem = document.getElementById("runtime");
-const movieDescriptionElem = document.getElementById("film-info");
-const screeningTimeElem = document.getElementById("showtime");
-const availableTicketsElem = document.getElementById("ticket-num");
-const buyButtonElem = document.getElementById("buy-ticket");
+const movieListElement = document.getElementById("films");
+const posterElement = document.getElementById("poster");
+const titleElement = document.getElementById("title");
+const durationElement = document.getElementById("runtime");
+const descriptionElement = document.getElementById("film-info");
+const showtimeElement = document.getElementById("showtime");
+const availableTicketsElement = document.getElementById("ticket-num");
+const buyButtonElement = document.getElementById("buy-ticket");
 
-//  removes all child elements from the element and replaces them with new child elements.
-filmListElem.replaceChildren();  
+// Removes all child elements from the element and replaces them with new child elements.
+movieListElement.replaceChildren();  
 
 // Function to fetch movie details from the server
-function fetchMovieDetails(id) {
-  fetch(`${baseURL}/${id}`)
+const fetchMovieDetails = (movieId) => {
+  fetch(`${filmsEndpoint}/${movieId}`)
     .then(response => response.json())
-    .then(cinema => {
-      displayMovieDetails(cinema);
+    .then(movie => {
+      displayMovieDetails(movie);
     })
     .catch(error => console.error('Error fetching movie details:', error));
 }
 
 // Function to display movie details on the webpage
-function displayMovieDetails(cinema) {
-  movieTitleElem.textContent = cinema.title;
-  durationElem.textContent = `${cinema.runtime} minutes`;
-  movieDescriptionElem.textContent = cinema.description;
-  screeningTimeElem.textContent = cinema.showtime;
-  moviePosterElem.src = cinema.poster;
+const displayMovieDetails = (movie) => {
+  titleElement.textContent = movie.title;
+  durationElement.textContent = `${movie.runtime} minutes`;
+  descriptionElement.textContent = movie.description;
+  showtimeElement.textContent = movie.showtime;
+  posterElement.src = movie.poster;
 
-  let ticketsRemaining = cinema.capacity - cinema.tickets_sold;
-  availableTicketsElem.textContent = ticketsRemaining;
+  let ticketsRemaining = movie.capacity - movie.tickets_sold;
+  availableTicketsElement.textContent = ticketsRemaining;
   
   updateTicketStatus(ticketsRemaining);
 }
 
 // Function to update the ticket count and handle the "Sold Out" status
-function updateTicketStatus(ticketsRemaining) {
-  buyButtonElem.addEventListener("click", (event) => {
+const updateTicketStatus = (ticketsRemaining) => {
+  buyButtonElement.addEventListener("click", (event) => {
     event.preventDefault();
 
     if (ticketsRemaining > 0) {
       ticketsRemaining--;
-      availableTicketsElem.textContent = ticketsRemaining;
-      updateTicketsStatusOnTheServer(ticketsRemaining);
+      availableTicketsElement.textContent = ticketsRemaining;
+      updateTicketsOnServer(ticketsRemaining);
       postNewTicket();
     } else {
-      buyButtonElem.innerHTML = "Sold Out";
-      buyButtonElem.disabled = true;
+      buyButtonElement.innerHTML = "Sold Out";
+      buyButtonElement.disabled = true;
     }
   });
 }
 
 // Function to update tickets_sold on the server
-function updateTicketsStatusOnTheServer(ticketsRemaining) {
+const updateTicketsOnServer = (ticketsRemaining) => {
   const id = 1; // Assuming you are always updating the details for movie with id 1
-  fetch(`${baseURL}/${id}`, {
+  fetch(`${filmsEndpoint}/${id}`, {
     method: "PATCH",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      tickets_sold: cinema.capacity - ticketsRemaining,
+      tickets_sold: movie.capacity - ticketsRemaining,
     }),
   })
     .then(response => response.json())
-    .then(updatedCinema => console.log(updatedCinema))
+    .then(updatedMovie => console.log(updatedMovie))
     .catch(error => console.error('Error updating ticket status:', error));
 }
 
 // Function to post new ticket to the tickets endpoint
-function postNewTicket() {
+const postNewTicket = () => {
   const id = 1; // Assuming you are always posting the ticket for movie with id 1
   fetch(`http://localhost:3000/tickets`, {
     method: "POST",
@@ -91,21 +91,21 @@ function postNewTicket() {
 }
 
 // Function to delete a film from the server
-function deleteCinemaDetails(id, cinemaItem) {
-  fetch(`${baseURL}/${id}`, { method: "DELETE" })
+const deleteMovieDetails = (movieId, listItem) => {
+  fetch(`${filmsEndpoint}/${movieId}`, { method: "DELETE" })
     .then(response => response.json())
     .then(details => {
       console.log(details);
-      cinemaItem.remove(); // Remove the film from the list after successful deletion
+      listItem.remove(); // Remove the film from the list after successful deletion
     })
     .catch(error => console.error('Error deleting movie:', error));
 }
 
 // Function to create a list item for a movie
-function createCinemaListItem(cinema) {
-  const cinemaItem = document.createElement("li");
-  cinemaItem.textContent = cinema.title.toUpperCase();
-  cinemaItem.style.color = "blue";
+const createMovieListItem = (movie) => {
+  const movieItem = document.createElement("li");
+  movieItem.textContent = movie.title.toUpperCase();
+  movieItem.style.color = "blue";
 
   // Create a delete button for each film
   const deleteButton = document.createElement("button");
@@ -114,30 +114,30 @@ function createCinemaListItem(cinema) {
   deleteButton.style.cursor = "pointer";
 
   deleteButton.addEventListener("click", () => {
-    deleteCinemaDetails(cinema.id, cinemaItem);
+    deleteMovieDetails(movie.id, movieItem);
   });
 
-  cinemaItem.append(deleteButton);
+  movieItem.append(deleteButton);
 
-  cinemaItem.addEventListener("click", () => {
-    fetchMovieDetails(cinema.id);
+  movieItem.addEventListener("click", () => {
+    fetchMovieDetails(movie.id);
   });
 
-  return cinemaItem;
+  return movieItem;
 }
 
 // Function to display a list of movies and add click event listeners to each movie item
-function MovieList() {
-  fetch(baseURL)
+const displayMovieList = () => {
+  fetch(filmsEndpoint)
     .then(response => response.json())
-    .then(cinemaList => {
-      cinemaList.forEach(cinema => {
-        const cinemaItem = createCinemaListItem(cinema);
-        filmListElem.append(cinemaItem);
+    .then(movieList => {
+      movieList.forEach(movie => {
+        const movieItem = createMovieListItem(movie);
+        movieListElement.append(movieItem);
       });
     })
     .catch(error => console.error('Error fetching movie list:', error));
 }
 
 // Initialize the movie list on page load
-MovieList();
+displayMovieList();
